0b99269379d5164a54f94fabb080d6f52194463b
BenoitMtr <43859784+BenoitMtr@users.noreply.github.com>
2018-12-16 12:27:58 +0100

    ajout changements interface

--- js/shooter.js
+++ js/shooter.js
@@ -220,22 +220,96 @@ class Bullet {
         this.y -= 10 * Math.sin(this.angle);
     }
 }
-/*
-class Joueur{
-  constructor(newX,newY, vie){
-    this.x=newX;
-    this.y=newY;
-    this.vie=vie;
-    this.height=assetsCharges.skin1.height/2;
-    this.width=assetsCharges.skin1.width/2;
-    console.log("skin.height : "+this.height+" skin width : "+this.width);
+
+class Joueur {
+  constructor(x, y, angle, vitesse, vie, tempsMinEntreTirsEnMillisecondes,couleur) {
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.v = vitesse;
+    this.vie = vie;
+    this.bullets = [];
+	this.couleur=couleur;
+    // cadenceTir en millisecondes = temps min entre tirs
+    this.delayMinBetweenBullets = tempsMinEntreTirsEnMillisecondes;
   }
+  
+  draw(ctx,couleur) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.translate(-10, -10);
+    ctx.fillStyle=couleur;
+    // corps
+    ctx.fillRect(0, 0, 20, 20);
+    // canon
+    //ctx.fillRect(-10, 9, 10, 2);
     
-  draw(ctx) {  
-    ctx.drawImage(assetsCharges.skin1, this.x, this.y,assetsCharges.skin1.width/2, assetsCharges.skin1.height/2);
-  }   
+    ctx.restore();
+    
+    this.drawBullets(ctx);
+
+  }
+  
+  drawBullets(ctx) {
+    for(let i = 0; i < this.bullets.length; i++) {
+      let b = this.bullets[i];
+      b.draw(ctx);
+      b.move(); 
+      if ((b.x < 0) || (b.y < 0) || (b.x > lc) || (b.y > hc))
+            this.removeBullet(b)
+
+    }
+  }
+  /*
+  move(mousepos) {
+        // 2) On dÃ©place la balle 
+    let dx = this.x - mousepos.x;
+    let dy = this.y - mousepos.y;
+    this.angle = Math.atan2(dy, dx);
+    
+    if (distance(this.x, this.y, mousepos.x, mousepos.y) >= 10) {
+        //ball.v = 0;
+        this.x -= this.v * Math.cos(this.angle);
+        this.y -= this.v * Math.sin(this.angle);
+    }
+  }
+  */
+   move(x,y,angle) {
+        // 2) On dÃ©place la balle 
+    this.x += x;
+    this.y += y;
+    this.angle = angle;
+    /*
+    if (distance(this.x, this.y, mousepos.x, mousepos.y) >= 100) {
+        //ball.v = 0;
+        this.x -= this.v * Math.cos(this.angle);
+        this.y -= this.v * Math.sin(this.angle);
+    }*/
+  }
+   addBullet(time) {
+     // si le temps écoulé depuis le dernier tir est > temps max alors on tire
+     var tempEcoule=0;
+     
+     if(this.lastBulletTime !== undefined) {
+       tempEcoule = time - this.lastBulletTime;
+       //console.log("temps écoulé = " + tempEcoule);
+     }
+     
+     if((this.lastBulletTime === undefined) || (tempEcoule> this.delayMinBetweenBullets)) {
+        this.bullets.push(new Bullet(this));
+        // on mémorise le dernier temps.
+        this.lastBulletTime = time;
+     }
+   }
+
+   removeBullet(bullet) {
+        let position = this.bullets.indexOf(bullet);
+        this.bullets.splice(position, 1);
+    }
 }
 
+/*
 class Projectile {
   constructor(){ 
     this.x=Math.floor((Math.random() * lc) + 1);
@@ -300,8 +374,8 @@ console.log("on est entrés dans la fonction");
             
     }
   });*/
-  joueur1 = new Joueur(100, 100, 4.75, 2, 100,10,20,20);
-  joueur2 = new Joueur(400, 400, 4.75, 2, 100,10,20,20);
+   joueur1 = new Joueur(100, 100, 4.75, 2, 100,"red");
+  joueur2 = new Joueur(500, 100, 4.75, 2, 100,"green");
   window.addEventListener('keydown', function(evt) {
     if(event.keyCode==32){
       inputStates.SPACE = true;
